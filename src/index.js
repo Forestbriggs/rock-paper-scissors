b482cb09bc240d1a4cdeeba7877749f20ffe8ef1
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-03-30 19:15:07 -0700

    finsihed basic UI for rock paper scissors

--- src/index.js
+++ src/index.js
@@ -1,65 +1,112 @@
 let userWins = 0;
 let computerWins = 0;
 
+const userDiv = document.querySelector('#userScore');
+const compDiv = document.querySelector('#compScore');
+const resBox = document.querySelector('.results');
+
+function updateScores() {
+    userDiv.textContent = userWins;
+    compDiv.textContent = computerWins;
+}
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const index = Math.floor(Math.random() * choices.length);
     return choices[index];
 };
 
-function playRound() {
-    const playerSelection = prompt("Rock, Paper, or Scissors?: ");
+function playRound(playerSelection) {
     const computerSelection = getComputerChoice();
 
     if (playerSelection.toLowerCase() === 'rock') {
         switch (computerSelection) {
             case 'rock':
-                return 'Tie!';
+                resBox.textContent = "Tie!"
+                return
             case 'paper':
                 computerWins++;
-                return 'You lose! Paper beats Rock.';
+                resBox.textContent = 'You lose! Paper beats Rock.';
+                return
             case 'scissors':
                 userWins++;
-                return 'You win! Rock beats Scissors';
+                resBox.textContent = 'You win! Rock beats Scissors';
+                return
         };
     } else if (playerSelection.toLowerCase() === 'paper') {
         switch (computerSelection) {
             case 'rock':
                 userWins++;
-                return 'You win! Paper beats Rock.';
+                resBox.textContent = 'You win! Paper beats Rock.';
+                return
             case 'paper':
-                return 'Tie!';
+                resBox.textContent = 'Tie!';
+                return
             case 'scissors':
                 computerWins++;
-                return 'You lose! Scissors beats Paper';
+                resBox.textContent = 'You lose! Scissors beats Paper';
+                return
         };
     } else if (playerSelection.toLowerCase() === 'scissors') {
         switch (computerSelection) {
             case 'rock':
                 computerWins++;
-                return 'You lose! Rock beats Scissors.';
+                resBox.textContent = 'You lose! Rock beats Scissors.';
+                return
             case 'paper':
                 userWins++;
-                return 'You win! Scissors beats Paper.';
+                resBox.textContent = 'You win! Scissors beats Paper.';
+                return
             case 'scissors':
-                return 'Tie!';
+                resBox.textContent = 'Tie!';
+                return
         };
     }
 };
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        const result = playRound();
-        console.log(result);
-    }
+// function playGame() {
+//     for (let i = 0; i < 5; i++) {
+//         const result = playRound();
+//         console.log(result);
+//     }
+// 
+//     if (userWins > computerWins) {
+//         console.log('You won!');
+//     } else if (userWins < computerWins) {
+//         console.log('You lost!');
+//     } else {
+//         console.log('Tie!')
+//     }
+// };
 
-    if (userWins > computerWins) {
-        console.log('You won!');
-    } else if (userWins < computerWins) {
-        console.log('You lost!');
-    } else {
-        console.log('Tie!')
-    }
-};
+// playGame();
+const buttons = document.querySelectorAll('button:not(#restart)');
+const restart = document.querySelector('#restart');
 
-playGame();
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        playRound(e.target.id);
+        updateScores();
+        if (userWins === 5 || computerWins === 5) {
+            resBox.textContent = `Game Over! ${userWins > computerWins ? 'You win!' : 'Computer won :('}`;
+
+            buttons.forEach((button) => {
+                button.disabled = true;
+            });
+        }
+    })
+});
+
+restart.addEventListener('click', () => {
+    userWins = 0;
+    computerWins = 0;
+    resBox.textContent = '';
+    updateScores();
+    buttons.forEach((button) => {
+        button.disabled = false;
+    });
+});
+
+//* on load \/
+
+updateScores();
